86738adf722548e0dc2da0645655b9278654680d
@kfern <>
2017-10-17 23:07:30 +0200

    Create a feathers app using feathers-cli

---
+++ feathers-app/src/hooks/populate-teams.js
@@ -0,0 +1,10 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+
+module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
+  return function populateTeams (hook) {
+    // Hooks can either return nothing or a promise
+    // that resolves with the `hook` object for asynchronous operations
+    return Promise.resolve(hook);
+  };
+};
---
+++ feathers-app/src/services/index.js
@@ -0,0 +1,7 @@
+const users = require('./users/users.service.js');
+const teams = require('./teams/teams.service.js');
+module.exports = function () {
+  const app = this; // eslint-disable-line no-unused-vars
+  app.configure(users);
+  app.configure(teams);
+};
