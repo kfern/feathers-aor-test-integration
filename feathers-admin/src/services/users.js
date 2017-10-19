e0a318544860af8141038d8078d4fad3943a8555
@kfern <>
2017-10-19 22:53:48 +0200

    Users seed (admin@test & user@test)

--- feathers-admin/src/services/users.js
+++ feathers-admin/src/services/users.js
@@ -4,7 +4,9 @@ import { List, Datagrid, TextField } from 'admin-on-rest';
 export const UsersList = (props) => (
     <List {...props}>
         <Datagrid>
-            <TextField source="_id" />
+            <TextField label= "_id" source="_id" />
+            <TextField label= "Name" source="name" />
+            <TextField label="E-mail" source="email" />
         </Datagrid>
     </List>
 );
--- feathers-app/src/index.js
+++ feathers-app/src/index.js
@@ -4,10 +4,42 @@ const app = require('./app');
 const port = app.get('port');
 const server = app.listen(port);
 
+const usersInit = function (){
+  return app.service('users').remove(null, {})
+    .then(function(){
+        return usersCreate();
+    });
+};
+
+const usersCreate = function (){
+  return Promise.all([
+    app.service('users').create({
+      name: 'Administrator',
+      email: 'admin@test',
+      password: 'admin'
+    }),
+    app.service('users').create({
+      name: 'Standard user', 
+      email: 'user@test',
+      password: 'user'
+    })
+  ]).then(function(){
+      logger.info('Users created:'); 
+      logger.info('Username: admin@test Password: admin');
+      logger.info('Username: user@test  Password: user');     
+  });
+    
+};
+
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
 server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+  usersInit().then(
+      logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+    )
+    .catch(function(error){
+      logger.error(error);
+    })
 );
